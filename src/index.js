46319e03801ccbe7a6bda280ea6ed4551cfb70be
daniel <danugarri@gmail.com>
2021-06-08 14:53:57 +0200

    uploading to github

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'daniel',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password === this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    const login= (<form action="#" onSubmit= {this.authorize}>
+      <p>chsss: una pista...mi nombre en minúsculas</p>
+    <input type='password' placeholder='Password'/>
+    <input type="submit"/>
+    </form>)
+    const contactInfo = (
+    
+        <ul>
+          <li>
+           <a href='mailto:danugarri@gmail.com'>danugarri@gmail.com </a> 
+          </li>
+          <li>
+          666228068
+          </li>
+        </ul>
+    )
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ?  'Contacto' :'Introduce la contraseña'} 
+        </h1>
+        {this.state.authorized ? contactInfo :login}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Contact />, 
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
