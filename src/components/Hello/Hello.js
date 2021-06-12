4734b75743b20024780710fedfc67837efa31812
daniel <danugarri@gmail.com>
2021-06-12 12:57:41 +0200

    add sweetalert library

--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './Hello.css'
+import './Hello.css';
+import swal from 'sweetalert'; //importamos la libería de sweetalert 
+//recordar instalar el paquete de node para seetalert usando npm instal sweetalert
 
 
   class Hello extends React.Component {
   handleEvent() {
-    alert(`Esta es una web app hecha con React.`);
+   swal(`Esta es una web app hecha con React.`);
   }
 
   render() {
     return (
-      <h1  onClick={this.handleEvent}>
+      <h2  onClick={this.handleEvent}>
         Hazme click
-      </h1>
+      </h2>
     );
   }
 }
